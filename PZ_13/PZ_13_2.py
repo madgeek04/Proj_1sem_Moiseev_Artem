f3586debff58fcd92230025aafffb1f42da80b48
madgeek04 <mad_geek@mail.ru>
2022-01-22 19:30:32 +0300

    Практическая №13

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,9 +1,7 @@
 # Практическая №13 №2
 # Составить генератор (yield), который выводит из строки только буквы.
 def finding_letters(n):
-    for i in n:
-        if i.isalpha():
-            yield i
+    yield from [i for i in n if i.isalpha()]
 
 
 print(''.join([o for o in finding_letters(input())]))
