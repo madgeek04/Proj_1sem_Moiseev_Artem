52e64c0fa26599c8e192655d39fa6148c0d037f4
madgeek04 <95250794+madgeek04@users.noreply.github.com>
2022-03-15 15:39:33 +0300

    Практическая №14

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,2 +1,2 @@
 import tldextract
-print(*[tldextract.extract(i).fqdn for i in open('spisok.txt').read().split('\n')], sep='\n')
+print(*[tldextract.extract(i.split(' ')[-1]).fqdn for i in open('spisok.txt').read().split('\n')], sep='\n')
